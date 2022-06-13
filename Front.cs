0318fbcf5bef74c87eb8e8601f3d19d605d4b800
JoaoPedroCordoba <jp.8pedro86@gmail.com>
2022-06-13 20:40:16 -0300

    teste

---
+++ Front.cs
@@ -0,0 +1,40 @@
+﻿string opcaoDesejada;
+
+Console.Clear();
+
+Console.WriteLine("--- Calculadora 4 operações ---\n");
+
+Console.WriteLine("[+] Soma");
+Console.WriteLine("[-] Subtração");
+Console.WriteLine("[*] Multiplicação");
+Console.WriteLine("[/] Divisão");
+Console.WriteLine("[%] Porcentagem");
+
+Console.Write("\nSelecione a operação desejada: ");
+
+opcaoDesejada = Console.ReadLine()!;
+
+switch (opcaoDesejada)
+{
+    case "+":
+        Codigo.CalcularSoma();
+        break;
+    case "-":
+        Codigo.CalcularSubtracao();
+        break;
+    case "*":
+        Codigo.CalcularMultiplicacao();
+        break;
+    case "/":
+        Codigo.CalcularDivisao();
+        break;
+     case "%":
+        Codigo.CalcularPorcentagem();
+        break;
+    default:
+        Console.WriteLine("Opção inválida.");
+        break;
+    
+}
+
+Console.WriteLine("\nObrigado por utilizar nosso programa.");
